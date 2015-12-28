6b399eb4df5cf2ed6faf5348fe074c49865b2c1f
tuxiantian <tuxiantian@163.com>
2015-12-28 20:14:28 +0800

    Update TestRegistryServiceImpl.java

--- dubbo_provider/src/main/java/test/dubbo/service/impl/TestRegistryServiceImpl.java
+++ dubbo_provider/src/main/java/test/dubbo/service/impl/TestRegistryServiceImpl.java
@@ -3,7 +3,11 @@ package test.dubbo.service.impl;
 import test.dubbo.service.TestRegistryService;
 
 public class TestRegistryServiceImpl implements TestRegistryService {
-
+	/**
+	 * hello
+	 * @param name
+	 * @return
+	 */
 	public String hello(String name) {
 		return "hello "+name;
 	}
